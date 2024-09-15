e151a7bdfa5c858059562582bf3297ffb4559ba5
iaman Lagares <iamancontatowork@gmail.com>
2024-09-15 19:42:20 -0300

    Add files via upload

---
+++ atividade1/atividade1.js
@@ -0,0 +1,12 @@
+
+const n1 = 100;
+const n2 = 10;
+
+
+const divResult = n1 / n2;
+const multResult = n1 * n2;
+const subResult = n1 - n2;
+const somaResult = n1 + n2;
+
+
+console.log(`O resultado da operação dos valores ${n1} e ${n2} é: \n Operação Soma: ${somaResult} \n Operação Subtração: ${subResult} \n Operação Multiplicação: ${multResult} \n Operação Divisão: ${divResult}`);
